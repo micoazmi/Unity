492341d3036e012baa813bbad4f1c52ac2e8fec3
micoazmi <micoazmi@yahoo.com>
2020-03-20 15:57:05 +0700

    Initial commit

---
+++ Assets/Scriptkanan.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scriptkanan : MonoBehaviour
+{
+	public int speed=20;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if(Input.GetKey(KeyCode.UpArrow)){
+    		GetComponent<Rigidbody2D>().velocity =new Vector2(0,1)*speed;
+
+    	}else if(Input.GetKey(KeyCode.DownArrow)){
+    		GetComponent<Rigidbody2D>().velocity =new Vector2(0,-1)*speed;
+
+    	}else {
+    		GetComponent<Rigidbody2D>().velocity =new Vector2(0,0)*speed;
+
+    	}
+    	
+    }
+}
---
+++ Assets/ball2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball2 : MonoBehaviour
+{	 public int speed=20;
+    	
+    // Start is called before the first frame update
+    void Start()
+    { Debug.Log("hello");
+    	GetComponent<Rigidbody2D>().velocity= new Vector2(1,-1)*speed;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D other){
+    if(other.collider.name=="kanan" || other.collider.name=="kiri"){
+        GetComponent<Transform>().position = new Vector2(0,0);
+    }
+    
+    }
+}
